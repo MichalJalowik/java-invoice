13893072151657705a7a4e3b3da0eb42ccb5ae80
MJ <michaljalowik@gmail.com>
2020-05-01 18:52:43 +0200

    v2

--- src/main/java/pl/edu/agh/mwo/invoice/Main.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Main.java
@@ -5,17 +5,16 @@ import pl.edu.agh.mwo.invoice.product.DairyProduct;
 import pl.edu.agh.mwo.invoice.product.OtherProduct;
 import pl.edu.agh.mwo.invoice.product.Product;
 
-
 public class Main {
     public static void main(String[] args){
         Invoice oneInv = new Invoice();
         Invoice secondInv = new Invoice();
-        oneInv.addProduct(new OtherProduct("piwo", new BigDecimal("3.0")),4);
-        oneInv.addProduct(new OtherProduct("wodeczka", new BigDecimal("30.0")),1);
-        secondInv.addProduct(new OtherProduct("ciasteczka", new BigDecimal("5.00")),3);
-        secondInv.addProduct(new OtherProduct("ciasteczka", new BigDecimal("5.00")),3);
-        secondInv.addProduct(new OtherProduct("pireniczkek", new BigDecimal("8.00")),1);
-        secondInv.addProduct(new OtherProduct("ciasteczka", new BigDecimal("5.00")),3);
+        oneInv.addProduct(new OtherProduct("piwo", new BigDecimal("3.00")), 4);
+        oneInv.addProduct(new OtherProduct("wodeczka", new BigDecimal("30.00")), 1);
+        secondInv.addProduct(new OtherProduct("ciasteczka", new BigDecimal("5.00")), 3);
+        secondInv.addProduct(new OtherProduct("ciasteczka", new BigDecimal("5.00")), 3);
+        secondInv.addProduct(new OtherProduct("pireniczkek", new BigDecimal("8.00")), 1);
+        secondInv.addProduct(new OtherProduct("ciasteczka", new BigDecimal("5.00")), 3);
         printInv(oneInv);
         printInv(secondInv);
     }
